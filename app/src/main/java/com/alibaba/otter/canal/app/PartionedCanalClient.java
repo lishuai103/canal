c54ad4ae516513e1e9c5e18e62821a9c853c3db0
Jian Wang <synckey@gmail.com>
2014-11-26 15:13:11 +0800

    convert sql type to avro type

--- app/src/main/java/com/alibaba/otter/canal/app/PartionedCanalClient.java
+++ app/src/main/java/com/alibaba/otter/canal/app/PartionedCanalClient.java
@@ -33,14 +33,15 @@ public class PartionedCanalClient extends AbstractCanalClient {
     private Schema schema;
     private static DataFileWriter<GenericRecord> writer = new DataFileWriter<GenericRecord>(new GenericDatumWriter());
     private static boolean inited = false;
+    private static File file;
 
     public PartionedCanalClient(String destination, String schemaPath) throws IOException {
         super(destination);
         this.schema = getSchemaFromFile(schemaPath);
         synchronized (writer) {
             if (!inited) {
-                File file = new File(schemaPath + ".data");
-                //writer.create(schema, file);
+                file = new File(schemaPath + ".data");
+                writer.create(schema, file);
                 inited = true;
                 logger.info("avro writer created");
             }
